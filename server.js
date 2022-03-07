249dfe0e4b94a5d0de546780e305c132ffa15070
Laura Walters <laura.walters1098@yahoo.com>
2022-03-07 22:32:23 +0000

    ammending file paths

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const port = 3000;
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.send("hello");
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`server is listening at ${port}`);
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening at ${PORT}`);
 });
