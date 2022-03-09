424ead62e7785510878fd2fcff4f67aabcea5239
Laura Walters <laura.walters1098@yahoo.com>
2022-03-09 23:25:51 +0000

    Adding save note functionality

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require("express");
+
 const app = express();
 const router = express.Router();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const fs = require("fs");
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+  console.log(req);
+  readAndAppend(req.body, "db/db.json");
+});
+
+app.get("api/notes", function (req, res) {
+  fs.readFile("db/db.json");
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
